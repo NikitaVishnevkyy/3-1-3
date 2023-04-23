62ce002bb6f0035de17a73f6c033d09641de796d
NikitaVishnevkyy <119518869+NikitaVishnevkyy@users.noreply.github.com>
2023-04-23 12:16:38 +0300

    Add files via upload

---
+++ 3.1.3/src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,78 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Role(Long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role; }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "role - " + role;
+    }
+
+    public String getNoPrefix() {
+        String prefix = "ROLE_";
+        return role.substring(prefix.length());
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+
+        Role role1 = (Role) object;
+
+        if (id != null ? !id.equals(role1.id) : role1.id != null) return false;
+        if (role != null ? !role.equals(role1.role) : role1.role != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+}
